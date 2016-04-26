6ffb63c97072958f9c85c95d61ba9101542ae9f1
Andy Staple <andy@staplewebdesign.com>
2016-04-26 15:19:07 -0400

    cleaning up comments, updating layout some

--- functions.php
+++ functions.php
@@ -1,12 +1,14 @@
-<?php // start php 
-function mytheme_enqueue() { //create a function that we can call as a hook
-	wp_register_script( 'site', get_template_directory_uri().'site.js', array( 'jquery' ) ); // locate jquery code we want to use in our theme
-	wp_enqueue_script( 'site' ); // add it to the list of resources to load
+<?php // start
 
-	wp_register_style( 'screen', get_stylesheet_uri(), '', '', 'screen' ); // locate css code we want to use in our theme
-	wp_enqueue_style( 'screen' ); // add it to the list of resources to load
-}
-add_action( 'wp_enqueue_scripts', 'mytheme_enqueue' ); // load css and javascript resources
 register_nav_menus(array('primary' => 'Primary Nav','footer' => 'Footer Nav')); //enable menus and create locations for them
 register_sidebar(array('id' => 'widgets','name' => 'Sidebar widgets')); // enable widgets in our theme that can be added to a sidebar
-// you don't need to close the php tag. Why? Who knows.
+
+
+// Enqueue items we want to add to the head here (js files, stylesheets, etc...)
+// A bit advanced, but a much more powerful way to add files to the head vs adding it to the header.php template directly
+
+// function mytheme_enqueue() { //create a function that we can call as a hook
+// 	wp_register_style( 'screen', get_stylesheet_uri(), '', '', 'screen' );
+// 	wp_enqueue_style( 'screen' );
+// }
+// add_action( 'wp_enqueue_scripts', 'mytheme_enqueue' );
--- header.php
+++ header.php
@@ -1,33 +1,44 @@
-<!-- header.php 
+<!-- header.php
 This file gets loaded by the command get_header(); and we see that on every page file, so front-page.php, page.php, archive.php are all loading this file. Whatever we put here will become part of those files. -->
-<!DOCTYPE html><!-- Let the world know this is an html file -->
-<html lang="en"><!-- Let the world know this site is in English -->
-<head><!-- The "brains" of the site; this element contains identifying info, loads scripts, and more stuff that you can't see. -->
-	<meta charset="UTF-8"><!-- The site uses the UTF-8 character set. -->
-	<meta name="viewport" content="width=device-width, initial-scale=1"><!-- enable touch zooming and ensure proper rendering on mobile devices -->
-	<!-- The title tag appears in your browser's tab, or in the user's history, or in a bookmark. It doesn't appear on the page. -->
-	<title>
-		<?php 
-			wp_title();
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+
+	<title><?php wp_title(); ?></title>
+
+	<link href="http://fonts.googleapis.com/css?family=Raleway:400,200,500,600,700,800,300" rel="stylesheet" /><!-- adds a webfont via google fonts -->
+
+	<link href="<?php echo get_stylesheet_uri(); ?>" rel="stylesheet" /><!-- links to the style.css file -->
+
+	<?php wp_head(); // This command loads additional resources needed ?>
+</head>
+
+<body <?php body_class(); ?>><!-- The "body" of the site; this element contains everything you can see. -->
+
+<div id="wrapper">
+
+	<div id="menu-wrapper">
+		<?php
+			wp_nav_menu(array(
+				'theme_location'	=> 'primary',
+				'container'				=> 'nav',
+				'container_id'		=> 'menu',
+				'container_class' => 'container nav-container'
+			));
+			// This block above displays the primary menu. We can manage these links in the WP-Admin>Appearance>Menus.
 		?>
-	</title>
-	<!-- All done displaying the title of the page -->
-	<link href="<!-- Link to style.css, the external stylesheet that will contain all the style and formatting rules for our site -->
-		<?php  // Use php to process the next line of code
-			echo get_stylesheet_uri(); // This command writes out an absolute link to style.css. We could hard-code it by actually typing out the url, but it would break if we moved our site to a different server.
-		?><!--  All done using php for now -->
-		" rel="stylesheet"><!-- All done loading the stylesheet -->
-	<?php // Use php to process the next line of code
-		wp_head(); // This command loads additional resources
-	?><!-- All done using php for now -->
-</head><!-- All done with the head -->
-<body <?php body_class(); ?><!-- The "body" of the site; this element contains everything you can see. -->
-<header>
-	<h1><?php 
-	bloginfo('name'); //Displays the site title you chose when you created your site. Can be changed in Settings>General
-	 ?></h1>
-</header>
-<?php // Use php to process the next line of code
-	wp_nav_menu(array('theme_location'=> 'primary', 'container'=>'nav')); // This command displays the primary menu. We can manage these links in the WP-Admin>Appearance>Menus.
-?><!-- All done using php for now -->
-<div id="pagecontent"><!-- All the page's unique content goes below here -->
+	</div>
+
+	<header id="header-wrapper">
+		<div id="header" class="container">
+			<h1>
+				<?php bloginfo('name');	//Displays the site title you chose when you created your site. Can be changed in Settings>General
+				?>
+			</h1>
+		</div>
+	</header>
+
+	<div id="banner"></div>
+	<div id="page" class="container">
