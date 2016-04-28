e6fd361ed562e031203fd14ab40aa89e7acdf484
Ben Dunkle <ben@field2.com>
2016-04-28 07:51:56 -0400

    fixed php error on front-page

--- front-page.php
+++ front-page.php
@@ -12,9 +12,9 @@ If you're using WordPress primarily as a CMS, consider creating a home page in P
 			while ( have_posts() ) : // There may be more than one post to display. "While" starts the loop, repeating until all post content has been displayed.
 				the_post(); // you need this. "Sets up the post", according to wp.org. OK.
 
-				<?php the_title('<h2 class="entry-title">','</h2>'); // This command dispays the title of the post ?>
+				the_title('<h2 class="entry-title">','</h2>'); // This command dispays the title of the post 
 
-				<?php the_content(); // This command displays the content of the post ?>
+				the_content(); // This command displays the content of the post 
 
 			endwhile; // Stop looping, because all the posts that were found with have_posts() have been displayed
 		else : // Maybe there aren't any posts? What if have_posts() didn't find anything? ?>
